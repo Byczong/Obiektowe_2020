c6935ebe5528cf15c6c242954859e5ccc4a32fc3
Byczong <piotrzwsln8@gmail.com>
2020-10-22 13:59:46 +0200

    Update Vector2d.java

--- Projekty/src/main/pl/edu/agh/po/lab02/Vector2d.java
+++ Projekty/src/main/pl/edu/agh/po/lab02/Vector2d.java
@@ -1,16 +1,24 @@
 package pl.edu.agh.po.lab02;
 
+import java.util.Objects;
+
 public class Vector2d {
 
-    // niemodyfikowalne (final) wspolrzedne x i y
     public final int x;
     public final int y;
 
-    //konstruktor ( przypisuje x i y )
     public Vector2d(int x, int y) {
         this.x = x;
         this.y = y;
     }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
     public boolean precedes(Vector2d other){
         return this.x <= other.x && this.y <= other.y;
     }
@@ -20,19 +28,29 @@ public class Vector2d {
 
     public Vector2d upperRight(Vector2d other){
         int newx, newy;
-        if (this.x >= other.x)  newx = this.x;
-        else                   newx = other.x;
-        if (this.y >= other.y)  newy = this.y;
-        else                   newy = other.y;
+        if (this.x >= other.x)
+            newx = this.x;
+        else
+            newx = other.x;
+
+        if (this.y >= other.y)
+            newy = this.y;
+        else
+            newy = other.y;
         return new Vector2d(newx, newy);
     }
 
     public Vector2d lowerLeft(Vector2d other){
         int newx, newy;
-        if (this.x <= other.x)  newx = this.x;
-        else                   newx = other.x;
-        if (this.y <= other.y)  newy = this.y;
-        else                   newy = other.y;
+        if (this.x <= other.x)
+            newx = this.x;
+        else
+            newx = other.x;
+
+        if (this.y <= other.y)
+            newy = this.y;
+        else
+            newy = other.y;
         return new Vector2d(newx, newy);
     }
 
@@ -48,22 +66,22 @@ public class Vector2d {
         return new Vector2d(-this.x, -this.y);
     }
 
-
-    // metoda equals - sprawdza czy obiekty klasy sa rowne
+    @Override
     public boolean equals(Object other) {
-        if (this == other) return true;
-        if (other == null || getClass() != other.getClass()) return false;
+        if (this == other)
+            return true;
+        if (other == null || getClass() != other.getClass())
+            return false;
         Vector2d v = (Vector2d) other;
         return x == v.x && y == v.y;
     }
-
-  // //hashcode - uzywa equals, stosuje sie do haszowania w strukturach
-  //public int hashCode() {
-  //    return Objects.hash(x, y);
-  //}
-
-    // toString
+    @Override
+    public int hashCode() {
+        return Objects.hash(x, y);
+    }
+    @Override
     public String toString() {
         return "(" + x + "," + y + ")";
     }
+
 }
